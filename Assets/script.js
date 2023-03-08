8ba3e63e7b8f09a142e112e910fc9a3fa8da8725
rhodemc <rhodemc@gmail.com>
2023-03-07 19:00:02 -0800

    zip code saves to local storage

--- Assets/script.js
+++ Assets/script.js
@@ -1,18 +1,26 @@
-const searchBtn = document.getElementById('search-btn');
-const resultsPanel = document.getElementById('results-panel');
+const searchBtn = document.getElementById("search-btn");
+const resultsPanel = document.getElementById("results-panel");
 
-searchBtn.addEventListener('click', function(event) {
-  event.preventDefault(); 
-  resultsPanel.classList.remove('hidden');
+searchBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  resultsPanel.classList.remove("hidden");
+
+  let zipInput = document.getElementById("zip").value;
+
+  if (!zipInput) {
+    console.error("You need to enter a zip code!");
+    return;
+  }
+
+  localStorage.setItem("zip", JSON.stringify(zipInput));
 });
 
 function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 8,
-      center: { lat: 37.7749, lng: -122.4194 },
-    });
-    map.setOptions({ draggable: true });
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 8,
+    center: { lat: 37.7749, lng: -122.4194 },
+  });
+  map.setOptions({ draggable: true });
 
-    console.log("MAP");
-  }
-
+  console.log("MAP");
+}
